849c7a2e10edf6903c95d38c60eb3943c2780714
Alexandru <lexyus22@gmail.com>
2026-06-23 10:36:28 +0200

    Read and iterate birthday data

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import pandas
+import pandas as pd
 import datetime as dt
 import smtplib
 import random
 
 now = dt.datetime.now()
+
+month = now.month
+day   = now.day
+
+data = pd.read_csv('birthdays.csv')
+
+for index, row in data.iterrows():
+    month = row.month
+    day = row.day
+    name = row.name
+    print(f'Name: {row['name']} | Day: {day} | Month: {month}')
 
