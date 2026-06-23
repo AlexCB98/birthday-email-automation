26bfa318f32359c0bafab9823f4dd668dc3bad95
Alexandru <lexyus22@gmail.com>
2026-06-23 11:32:24 +0200

    Complete birthday email automation project

--- main.py
+++ main.py
@@ -3,34 +3,34 @@ import datetime as dt
 import smtplib
 import random
 
+MY_EMAIL = 'email_1@gmail.com'
+PASSWORD = 'Pass123(-)'
+
+
 now = dt.datetime.now()
 current_month = now.month
 current_day   = now.day
 
 data = pd.read_csv('birthdays.csv')
 
-my_email = 'email_1@gmail.com'
-password = 'Pass123(-)'
-
-
 for index, row in data.iterrows():
-    birthday_month = row.month
-    birthday_day = row.day
-
-    random_letter = random.randint(1, 5)
-
-    with open(f'letter_templates/letter_{random_letter}.txt') as letter_file:
-        letter_content = letter_file.read()
-        personalized_letter = letter_content.replace('[NAME]', row['name'])
-
+    birthday_month = row['month']
+    birthday_day = row['day']
 
     if birthday_month == current_month and birthday_day == current_day:
+
+        random_letter = random.randint(1, 5)
+
+        with open(f'letter_templates/letter_{random_letter}.txt') as letter_file:
+            letter_content = letter_file.read()
+            personalized_letter = letter_content.replace('[NAME]', row['name'])
+
         with smtplib.SMTP('smtp.gmail.com') as connection:
             connection.starttls()
-            connection.login(user=my_email, password=password)
+            connection.login(user=MY_EMAIL, password=PASSWORD)
             connection.sendmail(
-                from_addr=my_email,
-                to_addrs='email_2@gmail.com',
+                from_addr=MY_EMAIL,
+                to_addrs=row['email'],
                 msg=f'Subject: Happy Birthday, {row['name']}\n\n'
                     f'{personalized_letter}'
             )
